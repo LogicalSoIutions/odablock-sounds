c8ed16b3e9c3796e7108071851f4d9e636b061bd
LogicalSoIutions <67069181+LogicalSoIutions@users.noreply.github.com>
2026-05-20 22:53:58 -0700

    reorder

--- src/main/java/com/github/dappermickie/odablock/Sound.java
+++ src/main/java/com/github/dappermickie/odablock/Sound.java
@@ -2,110 +2,103 @@ package com.github.dappermickie.odablock;
 
 public enum Sound
 {
-	REDEMPTION_PROC("redemption", "RedemptionProc_r1.wav"),//#1
-	REDEMPTION_PROC_2("redemption", "RedemptionProc_r2.wav"),//#1
-	REDEMPTION_PROC_3("redemption", "RedemptionProc_r3.wav"),//#1
-	REDEMPTION_PROC_4("redemption", "RedemptionProc_r4.wav"),//#1
+	// General Settings
+	WELCOME_SCREEN_SOUND("welcomescreen", "WelcomeScreen.wav"),
 
-	DEATH("death", "DyingHCIMCompleted_r1.wav"),//#2
-	COLLECTION_LOG_SLOT("collectionlog", "ColLogSlotCompleted_r1.wav"),//#3
-	SMITED_NO_PRAYER("smited", "SmitedNoPrayer.wav"),//#4
-	NEW_PET("newpet", "NewPet.wav"),//#5
-	PETTING_DOG("pettingdog", "PettingDog.wav"),//#6
-	DECLINE_TRADE("declinetrade", "DeclineTrade.wav"),//#7
-	VENGEANCE("vengeance", "Vengeance.wav"),//#8
-
-	KILLING_SOMEONE_1("playerkilling", "KillingSomeone_r1.wav"),//#9
-	KILLING_SOMEONE_2("playerkilling", "KillingSomeone_r2.wav"),//#9
-	KILLING_SOMEONE_3("playerkilling", "KillingSomeone_r3.wav"),//#9
-	KILLING_SOMEONE_4("playerkilling", "KillingSomeone_r4.wav"),//#9
-
-	REPORT_PLAYER_1("reportplayer", "ReportPlayer_r1.wav"),//#10
-	REPORT_PLAYER_2("reportplayer", "ReportPlayer_r2.wav"),//#10
-	REPORT_PLAYER_3("reportplayer", "ReportPlayer_r3.wav"),//#10
-
-	ZEBAK_ROAR("zebakroar", "ZebakRoar.wav"),//#12
-	RUBY_PROC("rubyproc", "RubyProc.wav"),//#13
-	//HIT_BY_KEPHRI("holykephri", "HitByKephri.wav"),//#14
-
-	COMBAT_TASK("combattask", "CombatTaskCompleted_r1.wav"),//#15
-
-
-	DDS_SPEC("ddsspec", "DdsSpec_r1.wav"),//#16
-	DDS_SPEC_2("ddsspec", "DdsSpec_r2.wav"),//#16
-	DDS_SPEC_3("ddsspec", "DdsSpec_r3.wav"),//#16
-
-	ACCEPTED_TRADE("accepttrade", "AcceptTrade.wav"),//#17
-
-	GETTING_PURPLE_1("gettingpurple", "GettingPurple_r1.wav"),//#18
-	GETTING_PURPLE_2("gettingpurple", "GettingPurple_r2.wav"),//#18
-
-
-	KILLING_RAT_OR_SCURRIUS_1("killingrat", "KillingRatOrScurrius_r1.wav"),//#18
-	KILLING_RAT_OR_SCURRIUS_2("killingrat", "KillingRatOrScurrius_r2.wav"),//#18
-
-	DISMISSING_RANDOM_EVENT("dismissrandomevent", "DismissingRandomEvent.wav"),//#20
-	DISMISSING_RANDOM_EVENT_2("dismissrandomevent", "Snowball_r1.wav"),
-	GETTING_RAGGED("gettingragged", "GettingRagged_r2.wav"),
-	TYPING_IN_BANKPIN("typingbankpin", "TypingInBankpin.wav"),//#21
-	CLIENT_DISCONNECTS("clientdisconnects", "ClientDisconnects.wav"),//#22
-	TOA_CHEST_OPENS("toachestopens", "ToaChestOpens.wav"),//#23
-	WHITE_LIGHT_AFTER_RAID("whitelight", "WhiteLightAfterRaid.wav"),//#24
-	TURNING_ON_RUN("turningonrun", "TurningOnRun.wav"),//#25
-	CLICKING_PK_LOOT_CHEST("pkchest", "ClickingPkLootChest.wav"),//#26
-
-	ACB_SPEC("acbspec", "AcbSpec_r1.wav"),//#27
-	ACB_SPEC_2("acbspec", "AcbSpec_r2.wav"),//#27
+	// Specs & Combat
+	RUBY_PROC("rubyproc", "RubyProc.wav"),
+	ZEBAK_ROAR("zebakroar", "ZebakRoar.wav"),
+	VENGEANCE("vengeance", "Vengeance.wav"),
+	DDS_SPEC("ddsspec", "DdsSpec_r1.wav"),
+	DDS_SPEC_2("ddsspec", "DdsSpec_r2.wav"),
+	DDS_SPEC_3("ddsspec", "DdsSpec_r3.wav"),
+	AGS_SPEC("agsspec", "AgsSpec_r1.wav"),
+	AGS_SPEC_2("agsspec", "AgsSpec_r2.wav"),
+	AGS_SPEC_3("agsspec", "AgsSpec_r3.wav"),
+	ACB_SPEC("acbspec", "AcbSpec_r1.wav"),
+	ACB_SPEC_2("acbspec", "AcbSpec_r2.wav"),
 	ACB_SPEC_3("acbspec", "AcbSpec_r3.wav"),
-
-	AGS_SPEC("agsspec", "AgsSpec_r1.wav"),//#28
-	AGS_SPEC_2("agsspec", "AgsSpec_r2.wav"),//#28
-	AGS_SPEC_3("agsspec", "AgsSpec_r3.wav"),//#28
-
-	DH_AXE_CHOP("dhchop", "DhAxe_chop_r1.wav"),//#29
+	DH_AXE_CHOP("dhchop", "DhAxe_chop_r1.wav"),
 	DH_AXE_CHOP_2("dhchop", "DhAxe_chop_r2.wav"),
-
-	DH_AXE_HACK("dhhack", "DhAxe_hack_r1.wav"),//#29
-	DH_AXE_HACK_2("dhhack", "DhAxe_hack_r2.wav"),//#29
-
-	DH_AXE_BLOCK("dhblock", "DhAxe_block_r1.wav"),//#29
-	DH_AXE_BLOCK_2("dhblock", "DhAxe_block_r2.wav"),//#29
+	DH_AXE_HACK("dhhack", "DhAxe_hack_r1.wav"),
+	DH_AXE_HACK_2("dhhack", "DhAxe_hack_r2.wav"),
+	DH_AXE_BLOCK("dhblock", "DhAxe_block_r1.wav"),
+	DH_AXE_BLOCK_2("dhblock", "DhAxe_block_r2.wav"),
 	DH_AXE_BLOCK_3("dhblock", "DhAxe_block_r3.wav"),
+	DH_AXE_SMASH("dhsmash", "DhAxe_smash_r1.wav"),
+	DH_AXE_SMASH_2("dhsmash", "DhAxe_smash_r2.wav"),
+	DH_AXE_SMASH_3("dhsmash", "DhAxe_smash_r3.wav"),
+	SMITED_NO_PRAYER("smited", "SmitedNoPrayer.wav"),
+	REDEMPTION_PROC("redemption", "RedemptionProc_r1.wav"),
+	REDEMPTION_PROC_2("redemption", "RedemptionProc_r2.wav"),
+	REDEMPTION_PROC_3("redemption", "RedemptionProc_r3.wav"),
+	REDEMPTION_PROC_4("redemption", "RedemptionProc_r4.wav"),
 
-	DH_AXE_SMASH("dhsmash", "DhAxe_smash_r1.wav"),//#29
-	DH_AXE_SMASH_2("dhsmash", "DhAxe_smash_r2.wav"),//#29
-	DH_AXE_SMASH_3("dhsmash", "DhAxe_smash_r3.wav"),//#29
+	// PvP / PvM
+	KILLING_SOMEONE_1("playerkilling", "KillingSomeone_r1.wav"),
+	KILLING_SOMEONE_2("playerkilling", "KillingSomeone_r2.wav"),
+	KILLING_SOMEONE_3("playerkilling", "KillingSomeone_r3.wav"),
+	KILLING_SOMEONE_4("playerkilling", "KillingSomeone_r4.wav"),
+	REPORT_PLAYER_1("reportplayer", "ReportPlayer_r1.wav"),
+	REPORT_PLAYER_2("reportplayer", "ReportPlayer_r2.wav"),
+	REPORT_PLAYER_3("reportplayer", "ReportPlayer_r3.wav"),
+	CLICKING_PK_LOOT_CHEST("pkchest", "ClickingPkLootChest.wav"),
+	SNOWBALL_1("snowball", "Snowball_r1.wav"),
+	SNOWBALL_2("snowball", "Snowball_r2.wav"),
+	FREEZE("freeze", "Freeze.wav"),
+	CRYPT("crypt", "Crypt.wav"),
+	FAHHHHH("fahhhhh", "Fahhhhh.wav"),
 
+	// Achievements & Milestones
 	LEVEL_UP("levelup", "LevelUpCompleted_r1.wav"),
+	REGULAR_GAMON("regulargamon", "Regular_Gamon.wav"),
+	Big_Gamon("biggamon", "Big_Gamon.wav"),
 	QUEST("quest", "QuestCompleted_r1.wav"),
+	COLLECTION_LOG_SLOT("collectionlog", "ColLogSlotCompleted_r1.wav"),
 	ACHIEVEMENT_DIARY("achievementdiary", "AchievementDiary_r1.wav"),
 	ACHIEVEMENT_DIARY_2("achievementdiary", "AchievementDiary_r2.wav"),
+	COMBAT_TASK("combattask", "CombatTaskCompleted_r1.wav"),
+	DEATH("death", "DyingHCIMCompleted_r1.wav"),
+	NEW_PET("newpet", "NewPet.wav"),
+	KILLING_RAT_OR_SCURRIUS_1("killingrat", "KillingRatOrScurrius_r1.wav"),
+	KILLING_RAT_OR_SCURRIUS_2("killingrat", "KillingRatOrScurrius_r2.wav"),
+
+	// Trades & Interactions
+	ACCEPTED_TRADE("accepttrade", "AcceptTrade.wav"),
+	DECLINE_TRADE("declinetrade", "DeclineTrade.wav"),
+	DISMISSING_RANDOM_EVENT("dismissrandomevent", "DismissingRandomEvent.wav"),
+	DISMISSING_RANDOM_EVENT_2("dismissrandomevent", "Snowball_r1.wav"),
+	GETTING_RAGGED("gettingragged", "GettingRagged_r2.wav"),
+	PHOENIX_NECKLACE("phoenixnecklace", "PhoenixNecklace.wav"),
+	PETTING_DOG("pettingdog", "PettingDog.wav"),
 	EASTER_EGG_STRAYDOG_BONE("givebone", "GiveBone.wav"),
-	Big_Gamon("biggamon", "Big_Gamon.wav"),
-	REGULAR_GAMON("regulargamon", "Regular_Gamon.wav"),
 	HAIRDRESSER_SOUND_1("hairdresser", "Hairdresser_r1.wav"),
 	HAIRDRESSER_SOUND_2("hairdresser", "Hairdresser_r2.wav"),
 	HAIRDRESSER_SOUND_3("hairdresser", "Hairdresser_r3.wav"),
-
-	SNOWBALL_1("snowball", "Snowball_r1.wav"),
-	SNOWBALL_2("snowball", "Snowball_r2.wav"),
-
+	TURNING_ON_RUN("turningonrun", "TurningOnRun.wav"),
+	TYPING_IN_BANKPIN("typingbankpin", "TypingInBankpin.wav"),
 	SPELLBOOK_SWAP_1("spellbookswap", "SpellbookSwap_r1.wav"),
 	SPELLBOOK_SWAP_2("spellbookswap", "SpellbookSwap_r2.wav"),
 	SPELLBOOK_SWAP_3("spellbookswap", "SpellbookSwap_r3.wav"),
 	SPELLBOOK_SWAP_4("spellbookswap", "SpellbookSwap_r4.wav"),
 
-	ODAS_ALERT("odasalert", "OdasAlert.wav"),
+	// Tombs of Amascut
+	WHITE_LIGHT_AFTER_RAID("whitelight", "WhiteLightAfterRaid.wav"),
+	GETTING_PURPLE_1("gettingpurple", "GettingPurple_r1.wav"),
+	GETTING_PURPLE_2("gettingpurple", "GettingPurple_r2.wav"),
+	TOA_CHEST_OPENS("toachestopens", "ToaChestOpens.wav"),
 
-	FREEZE("freeze", "Freeze.wav"),
-	CRYPT("crypt", "Crypt.wav"),
-	WELCOME_SCREEN_SOUND("welcomescreen", "WelcomeScreen.wav"),
-	PHOENIX_NECKLACE("phoenixnecklace", "PhoenixNecklace.wav"),
-	FAHHHHH("fahhhhh", "Fahhhhh.wav"),
+	// Music & Emotes
+	WARRIOR("warriors", "odablock_warriors.wav"),
 
+	// Livestream
 	LIVESTREAM("livestream", "Livestream.wav"),
 
-	WARRIOR("warriors", "odablock_warriors.wav");
+	// Notifications
+	ODAS_ALERT("odasalert", "OdasAlert.wav"),
+
+	// Other
+	CLIENT_DISCONNECTS("clientdisconnects", "ClientDisconnects.wav");
 
 	private final String resourceName;
 	private final String directory;
